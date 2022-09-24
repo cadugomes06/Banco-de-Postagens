fd9d4613f62964d8971f872af99ceb32185592d8
Carlos Eduardo <cadu.010808@gmail.com>
2022-09-24 10:31:13 -0300

    nova categoria de Postagens, utilizando as categorias

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const mongoose = require('mongoose')
 require('../models/Categoria')
 const Categoria = mongoose.model("categorias")
+require("../models/Postagem")
+const Postagem = mongoose.model('postagens')
 
 router.get('/', (req, res) => {
     res.render('admin/index')
@@ -65,7 +67,6 @@ router.get('/categorias/edit/:id', (req, res) => {
 })
 
 router.post("/categorias/edit", (req, res) => {
-
     Categoria.findOne({_id: req.body.id}).then((categoria) => {
         categoria.nome = req.body.nome
         categoria.slug = req.body.slug
@@ -93,4 +94,59 @@ router.post('/categorias/deletar', (req, res) => {
     })
 })
 
+router.get("/postagens", (req, res) => {
+    res.render("admin/postagens")
+})
+
+router.get("/postagens/add", (req, res) => {
+    Categoria.find().lean().then((categorias) => {
+        res.render("admin/addpostagem", {categorias: categorias})
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao carregar o formulário.")
+    })
+})
+
+router.post("/postagens/nova", (req, res) => {
+
+    var erros = []
+
+    if(!req.body.titulo || typeof req.body.titulo == undefined || req.body.titulo == null) {
+        erros.push({texto: "titulo inválido!"})
+    }
+    if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null) {
+        erros.push({texto: "slug inválido!"})
+    }
+    if(!req.body.descricao || typeof req.body.descricao == undefined || req.body.descricao == null) {
+        erros.push({texto: "descricao inválido!"})
+    }
+    if(!req.body.conteudo || typeof req.body.conteudo == undefined || req.body.conteudo == null) {
+        erros.push({texto: "conteudo inválido!"})
+    } 
+    if(req.body.categoria === '0') {
+        erros.push({texto: "Categoria inválida, registre uma categoria"})
+    } 
+    if(erros.length > 0) {
+        res.render("admin/postagens", {erros: erros})
+    } else {
+        const novaPostagem = {
+            titulo: req.body.titulo,
+            descricao: req.body.descricao,
+            slug: req.body.slug,
+            conteudo: req.body.conteudo,
+            categoria: req.body.categoria
+        }
+
+        new Postagem(novaPostagem).save().then(() => {
+            req.flash("success_msg", "Postagem criada com sucesso!")
+            res.redirect('/admin/postagens')
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro durante a postagem")
+            res.redirect('/admin/postagens')
+        })
+    }
+
+})
+
 module.exports = router
+
+//AULA 45 | ver - 46
